79b1c761aeab0c1719a6b6677a001d72b6050bee
ayeshaali <aliayeshanyc@gmail.com>
2018-05-11 10:26:21 -0400

    message

---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);
+    }
+}
