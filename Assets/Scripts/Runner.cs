5fc0a10c3a7d61ede5639609b48d8b57caa3717f
ayeshaali <aliayeshanyc@gmail.com>
2018-05-12 18:53:16 -0400

    message

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Runner : MonoBehaviour
-{
+public class Runner : MonoBehaviour {
 
-    void Update()
-    {
+    public static float distanceTraveled;
+    public float acceleration;
+    private bool touchingPlatform;
+    public Vector3 jumpVelocity;
+
+    void Update() {
+        if (touchingPlatform && Input.GetButtonDown("Jump")) {
+            GetComponent<Rigidbody>().AddForce(jumpVelocity, ForceMode.VelocityChange);
+            touchingPlatform = false;
+        }
         transform.Translate(5f * Time.deltaTime, 0f, 0f);
+        distanceTraveled = transform.localPosition.x;
+    }
+
+    void FixedUpdate(){
+        if (touchingPlatform)
+        {
+            GetComponent<Rigidbody>().AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+        }
+    }
+
+    void OnCollisionEnter() {
+        touchingPlatform = true;
+    }
+
+    void OnCollisionExit() {
+        touchingPlatform = false;
     }
 }
